2e1153e8c6d19187c9e88cb12f0182cdc720c2e3
Mario Ponce 230 <mario.ponce.230@beliveo.corp>
2019-05-18 09:16:44 -0500

    Se agrega Array para tabla.

--- 8b10b.sv
+++ 8b10b.sv
@@ -1,1 +1,22 @@
-first commit
+module serialization(clk,rst,data_in,data_out);
+    input clk,rst;
+    input [7:0] data_in;
+    output [9:0] data_out;
+    wire flag;
+        reg [5:0] 5b/6b [31:0] [1:0];
+        reg [3:0] 5b/6b [16:0] [1:0];
+
+    always@(posedge clk)
+        begin
+            if(flag == 1)
+                begin
+                    for ( = ; ; ) begin
+                        
+                    end
+                end
+        end
+
+
+
+
+endmodule
